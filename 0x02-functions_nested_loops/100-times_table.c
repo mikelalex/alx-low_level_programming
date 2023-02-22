6f4db90a41802b2f2cb934158db75a015d865acc
“mikelalex” <mikelalex20@gmail.com>
2023-02-21 21:04:23 -0800

    “100-times_table.c”

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,7 +6,6 @@
 void print_times_table(int n)
 {
 	int i, j, k;
-
 		if (n >= 0 && n <= 15)
 		{
 			for (i = 0; i <= n; i++)
@@ -14,8 +13,7 @@ void print_times_table(int n)
 				for (j = 0; j <= n; j++)
 				{
 					k = j * i;
-
-						if (j == 0)
+					if (j == 0)
 					{
 						_putchar(k + '0');
 					} else if (k < 10 && j != 0)
